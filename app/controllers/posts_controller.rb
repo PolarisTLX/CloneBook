c7a9d19cab3cb9c39dd7b76c5390bc79939b6a59
Kyle Lemon <james.kyle.lemon@gmail.com>
2018-05-28 10:14:29 -0400

    Posts show and edit, plus home link and flashes
    Co-authored-by: Paul Rail <paul.a.rail@gmail.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,32 @@ class PostsController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @post = Post.new
     @posts = Post.all
+  end
+
+  def create
+    @post = Post.new(user_id: current_user.id, content: params[:post][:content])
+    if @post.save
+      flash[:post_success] = 'Post successful!'
+      redirect_to root_url
+    else
+      render 'index'
+    end
+  end
+
+  def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find(params[:id])
+    if @post.update_attributes(content: params[:post][:content])
+      flash[:success] = "Post updated!"
+      redirect_to @post
+    else
+      render 'edit'
+    end
   end
 
   def show
--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,5 @@ class Post < ApplicationRecord
 
   validates :content, presence: true
 
+  default_scope -> { order(created_at: :desc) }
 end
