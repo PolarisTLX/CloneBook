e2009a98c2961c7859199468a2ef580c70fbdcb7
PolarisTLX <paul.a.rail@gmail.com>
2018-05-29 11:33:26 -0400

    Created comment button with javascript for their fields + seeding comments
    Co-authored-by: Kyle Lemon <james.kyle.lemon@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,12 +10,24 @@ users = User.create([
   { first_name: 'Kyle',  last_name: 'Lemon',  email: 'Kyle@email.com',  password: 'password' },
   { first_name: 'Paul',  last_name: 'Rail',   email: 'Paul@email.com',  password: 'password' },
   { first_name: 'Ariel', last_name: 'Camus',  email: 'Ariel@email.com', password: 'password' },
-  { first_name: 'Kevin', last_name: 'Wahome', email: 'Kevin@email.com', password: 'password' }])
+  { first_name: 'Kevin', last_name: 'Wahome', email: 'Kevin@email.com', password: 'password' },
+  { first_name: 'Alvaro', last_name: 'Diaz', email: 'Alvaro@email.com', password: 'password' }])
 
 users = User.all
-5.times do
-  content = Faker::Lorem.sentence(5)
-  users.each { |user| user.posts.create!(content: content) }
+# 5 posts for each user:
+3.times do
+  # content = Faker::Lorem.sentence(5)
+  users.each { |user| user.posts.create!(content: Faker::Lorem.sentence(5)) }
+end
+
+# each post have random number of comments (between 0 - 4)
+posts = Post.all
+posts.each do |post|
+  rand(0..4).times do
+    # content = Faker::Lorem.sentences
+    # assign random user id from 1-5 (there is no 0)
+    post.comments.create!(user_id: rand(1..5), content: Faker::Lorem.sentence)
+  end
 end
 
 # events = Event.create([{ title: "Something long enough", host_id: 1, description: "Text goes here, I hope.",              location: "Somewhere", date: "2018-08-01" },
