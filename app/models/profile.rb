7ad5ddbfeded9a28ec330cc6a16ba15f4a1a6b02
Kyle Lemon <james.kyle.lemon@gmail.com>
2018-05-31 16:58:18 -0400

    default images for users and sizing in html
    Co-authored-by: Paul Rail <paul.a.rail@gmail.com>

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -3,7 +3,7 @@ class Profile < ApplicationRecord
 
   # for paperclip for file attachment functionality:
   has_attached_file :avatar, styles: { medium: "300x300#", thumb: "100x100#", tac: "20x20#" },
-  default_url: '/images/:attachment/:style/missing.png'
+                             default_url: ActionController::Base.helpers.image_path("missing.png")
 
   validates_attachment_content_type :avatar, content_type: ["image/jpg", "image/jpeg", "image/png"]
 
