76992d3473d948583eef91a957feb1303203b64d
Kyle Lemon <james.kyle.lemon@gmail.com>
2018-06-08 12:22:15 -0400

    cleanup unused helpers and change requests_controller to use @friend_request

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,7 +1,7 @@
 class RequestsController < ApplicationController
   def create
-    @request = current_user.sent_requests.build(request_params)
-    if @request.save
+    @friend_request = current_user.sent_requests.build(friend_request_params)
+    if @friend_request.save
       flash[:success] = 'Friend request sent!'
       redirect_to root_url
     else
@@ -11,19 +11,19 @@ class RequestsController < ApplicationController
 
   def update
     # @request = current_user.received_requests.find_by(requester_id: params[:request][:requester_id])
-    @request = Request.find(params[:id])
-    @request.update_columns(accepted: 1)
+    @friend_request = Request.find(params[:id])
+    @friend_request.update_columns(accepted: 1)
     flash[:success] = 'Friend Request Accepted!'
     redirect_to User.find(params[:request][:requester_id]).profile
   end
 
   def index
-    @users = current_user.requesters.where('requests.accepted = ?', 0)    
+    @users = current_user.requesters.where('requests.accepted = ?', 0)
   end
 
   private
 
-  def request_params
+  def friend_request_params
     params.require(:request).permit(:requestee_id)
   end
 end
