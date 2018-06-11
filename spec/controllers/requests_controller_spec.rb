97abe101e362ae5ffbd8f375425f934efac6dd12
PolarisTLX <paul.a.rail@gmail.com>
2018-06-11 14:47:12 -0400

    Finished request controller rspec
    Co-authored-by: Kyle Lemon <james.kyle.lemon@gmail.com>

--- spec/controllers/requests_controller_spec.rb
+++ spec/controllers/requests_controller_spec.rb
@@ -10,64 +10,70 @@ RSpec.describe RequestsController, type: :controller do
   let(:other_user) { User.where('id NOT IN (?)', user.friend_ids).first }
   let(:third_user) { User.where('id NOT IN (?)', user.friend_ids).second }
 
-  # before(:all) do
-  #   third_user.sent_requests.create(requestee_id: user.id)
-  # end
-
   context 'when user is logged in' do
 
     before do
       sign_in(user)
     end
 
+    describe "GET #index" do
+      it 'shows all friend requests page' do
+          get :index
+          expect(response).to have_http_status(:success)
+      end
+    end
+
 
     describe 'POST #create' do
-
-      describe 'when new friend request is sent' do
-        it 'creates a new pending request' do
+        it 'clicking on "Add Friend" creates a new pending request' do
           expect { post :create, params: { request: { requester_id: user.id, requestee_id: other_user.id, accepted: 0 } }}.to change{Request.count}.by(1)
           expect(user.requestees).to include(other_user)
           expect(response).to redirect_to(root_url)
         end
-      end
-
-      # describe 'when new post does not have any content' do
-      #   it 'does NOT create a new post' do
-      #     expect{post :create, params: { post: { content: ''} }}.to change{Post.count}.by(0)
-      #   end
-      # end
-
     end
 
 
-  #
     describe 'PATCH #update' do
-      it 'updates after requestee accepts the request and redirects to ...' do
+      it 'updates after requestee accepts the request and redirects to the requester\'s profile' do
           third_user.sent_requests.create(requestee_id: user.id)
           expect { patch :update, params: { id: user.received_requests.where('requester_id = ?', third_user.id).first.id,
                                    request: { requester_id: third_user.id, requestee_id: user.id } } }.to change{user.friends.count}.by(1)
           expect(user.friends).to include(third_user)
+          expect(response).to redirect_to(third_user.profile)
       end
     end
-  #
-  #
-  # end
-  #
-  # context 'when user is NOT logged in' do
-  #
 
-  #
-  #   describe 'PATCH #update' do
-  #     it 'does NOT update the profile and redirects to login' do
-  #       patch :update, params: { id: user.id, profile: { gender: 'Male', location: 'Somewhere', bio: 'A little bit about me' } }
-  #       expect(user.profile.gender).to be nil
-  #       expect(user.profile.location).to be nil
-  #       expect(user.profile.bio).to be nil
-  #       expect(response).to redirect_to(new_user_session_path)
-  #     end
-  #   end
   end
 
+  context 'when user is NOT logged in' do
+
+    describe 'GET #index' do
+      it 'redirects to login page' do
+        get :index
+        expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+    describe 'POST #create' do
+        it 'a new pending request is not created' do
+          expect { post :create, params: { request: { requester_id: user.id, requestee_id: other_user.id, accepted: 0 } }}.to change{Request.count}.by(0)
+          expect(user.requestees).to_not include(other_user)
+          expect(response).to redirect_to(new_user_session_path)
+        end
+    end
+
+
+    describe 'PATCH #update' do
+      it 'A friend request cannot be accepted' do
+          third_user.sent_requests.create(requestee_id: user.id)
+          expect { patch :update, params: { id: user.received_requests.where('requester_id = ?', third_user.id).first.id,
+                                   request: { requester_id: third_user.id, requestee_id: user.id } } }.to change{user.friends.count}.by(0)
+          expect(user.friends).to_not include(third_user)
+          expect(response).to redirect_to(new_user_session_path)
+      end
+    end
+
+  end
 
 
 end
