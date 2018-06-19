9e6258940dd5d2ae20f2ce2872572b741d4e5e5b
PolarisTLX <paul.a.rail@gmail.com>
2018-06-19 16:31:51 -0400

    ordered all users by first name
    Co-authored-by: Kyle Lemon <james.kyle.lemon@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def index
     # @users = User.all
-    @users = User.where('id != ?' , current_user.id).page(params[:page]).per(12)
+    @users = User.where('id != ?' , current_user.id).order(first_name: 'asc').page(params[:page]).per(12)
   end
 
 end
