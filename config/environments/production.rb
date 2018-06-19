4c4873735b525b50106dbd6b40ca2fbd24d96f6b
Kyle Lemon <james.kyle.lemon@gmail.com>
2018-06-19 16:13:36 -0400

    amazon s3 east 1 for sure
    Co-authored-by: Paul Rail <paul.a.rail@gmail.com>

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -98,8 +98,7 @@ Rails.application.configure do
       bucket: ENV.fetch('S3_BUCKET_NAME'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
-      s3_host_name: 's3.us-east-2.amazonaws.com'
-      # s3_region: ENV.fetch('AWS_REGION'),
+      s3_region: ENV.fetch('AWS_REGION')
     }
   }
 
